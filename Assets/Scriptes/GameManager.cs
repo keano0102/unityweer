ccc35ec288b7d5b76451337661e1bbdbd8e99acc
keano0102 <keano.pittevils@student.arcadiascholen.be>
2023-12-02 14:43:53 +0100

    11th commit
    proberen nu de paddles te resetten na een goal en het scoreboard te maken

--- Assets/Scriptes/GameManager.cs
+++ Assets/Scriptes/GameManager.cs
@@ -1,23 +1,33 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Ball ball;
+    public Paddle playerPaddle;
+    public Paddle computerPaddle;
+    public Text playerScoreText;
+    public Text computerScoreText;
+
     private int _playerScore;
     private int _computerScore;
 
     public void PlayerScore()
     {
         _playerScore++;
-        Debug.Log(_playerScore);
+
+        this.playerScoreText.text = _playerScore.ToString();
         this.ball.ResetPosition();
+        this.ball.AddStartingForce();
     }
     public void ComputerScore()
     {
         _computerScore++;
-        Debug.Log (_computerScore);
+
+        this.computerScoreText.text = _computerScore.ToString();
         this.ball.ResetPosition();
+        this.ball.AddStartingForce();
     }
 
 }
